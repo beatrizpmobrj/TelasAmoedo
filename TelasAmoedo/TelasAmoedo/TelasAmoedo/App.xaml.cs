c252d117e684bda525b475e07ae893208fb82f6b
HigorPereira <higor.pereira@mobrj.com.br>
2022-04-07 11:33:42 -0300

    Trabalhando na tela de login

--- TelasAmoedo/TelasAmoedo/TelasAmoedo/App.xaml.cs
+++ TelasAmoedo/TelasAmoedo/TelasAmoedo/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TelasAmoedo.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace TelasAmoedo
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Login();
         }
 
         protected override void OnStart()
