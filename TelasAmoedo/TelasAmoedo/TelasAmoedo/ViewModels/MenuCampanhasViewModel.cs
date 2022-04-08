9cb2d720a4db7394b5849ae5eb8b4990a25f4d85
Beatriz Pinheiro <beatriz.pinheiro@mobrj.com.br>
2022-04-08 14:51:33 -0300

    Desenvolvendo tela de menu de campanhas usando Collection View

---
+++ TelasAmoedo/TelasAmoedo/TelasAmoedo/ViewModels/MenuCampanhasViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using TelasAmoedo.Models;
+
+namespace TelasAmoedo.ViewModels
+{
+    public class MenuCampanhasViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<ItemsMenuCampanhas> Itens { get; set; }
+
+        public MenuCampanhasViewModel()
+        {
+            Itens = new ObservableCollection<ItemsMenuCampanhas>();
+            Itens.Add(new ItemsMenuCampanhas { Nome = "Campanha 06"});
+            Itens.Add(new ItemsMenuCampanhas { Nome = "Campanha 05"});
+            Itens.Add(new ItemsMenuCampanhas { Nome = "Campanha 04"});
+            Itens.Add(new ItemsMenuCampanhas { Nome = "Campanha 03"});
+            Itens.Add(new ItemsMenuCampanhas { Nome = "Campanha 02"});
+            Itens.Add(new ItemsMenuCampanhas { Nome = "Campanha 01"});
+
+        }
+    }
+}
+
