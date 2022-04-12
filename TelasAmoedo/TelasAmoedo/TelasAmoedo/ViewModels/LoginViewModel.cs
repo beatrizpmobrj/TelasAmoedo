2dba3170af762e48d3bf88fa3a1425c9be7f10e0
HigorPereira <higor.pereira@mobrj.com.br>
2022-04-12 08:41:03 -0300

    Pegando a digital no android

--- TelasAmoedo/TelasAmoedo/TelasAmoedo/ViewModels/LoginViewModel.cs
+++ TelasAmoedo/TelasAmoedo/TelasAmoedo/ViewModels/LoginViewModel.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using Plugin.Fingerprint;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using Plugin.Fingerprint.Abstractions;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace TelasAmoedo.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : Shell
     {
+        public ICommand LeitorBiometricoCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            LeitorBiometricoCommand = new Command(IsDigitalValidada);
+        }
+
+        private async void IsDigitalValidada()
+        {
+            var availability = await CrossFingerprint.Current.IsAvailableAsync();
+
+            if(!availability)
+            {
+                await DisplayAlert("Erro!", "Leitor biométrico não disponível.", "OK");
+                return;
+            }
+
+            var authResult = await CrossFingerprint.Current.AuthenticateAsync(
+                new AuthenticationRequestConfiguration("Acesso Biométrico", "Confirme sua impressão digital para acessar sua conta."));
+
+            if (authResult.Authenticated)
+            {
+                await DisplayAlert("Pronto!", "Acesso liberado!", "OK");
+            }
+        }
     }
 }
