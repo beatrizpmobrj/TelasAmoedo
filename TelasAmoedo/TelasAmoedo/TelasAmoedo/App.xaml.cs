0c3d100bcab53aee7cbc73b597d466b31f6163ca
HigorPereira <higor.pereira@mobrj.com.br>
2022-04-13 13:57:38 -0300

    Navegação da pagina Cadastro para Login no botão Avançar

--- TelasAmoedo/TelasAmoedo/TelasAmoedo/App.xaml.cs
+++ TelasAmoedo/TelasAmoedo/TelasAmoedo/App.xaml.cs
@@ -29,6 +29,7 @@ namespace TelasAmoedo
             //Routing.RegisterRoute("menuprincipalpage", typeof(MenuPrincipal));
             Routing.RegisterRoute("confirmacaotelefonepage", typeof(ConfirmacaoTelefone));
             Routing.RegisterRoute("confirmacaocodigopage", typeof(ConfirmacaoCodigo));
+            //Routing.RegisterRoute("loginpage", typeof(Login));
         }
 
         protected override void OnStart()
