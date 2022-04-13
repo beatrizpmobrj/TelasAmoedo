702d765c527dae87f537246d720ed4b372d78047
HigorPereira <higor.pereira@mobrj.com.br>
2022-04-13 13:46:02 -0300

    Botão voltarda tela de ConfirmacaoCodigo levando para a tela de ConfirmacaoTelefone

--- TelasAmoedo/TelasAmoedo/TelasAmoedo/ViewModels/ConfirmacaoCodigoViewModel.cs
+++ TelasAmoedo/TelasAmoedo/TelasAmoedo/ViewModels/ConfirmacaoCodigoViewModel.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace TelasAmoedo.ViewModels
 {
     public class ConfirmacaoCodigoViewModel
     {
+        public ICommand VoltarCommand { get; set; }
+        public ConfirmacaoCodigoViewModel()
+        {
+            VoltarCommand = new Command(async () => await BotaoVoltar());
+        }
+
+        private async Task BotaoVoltar()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
     }
 }
