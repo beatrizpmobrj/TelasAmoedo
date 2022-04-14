e5f70452090c3a946ab06804db6a1145b6b8ee88
Beatriz Pinheiro <beatriz.pinheiro@mobrj.com.br>
2022-04-14 10:24:46 -0300

    Realização do Binding com os itens da lista do menu voucher

--- TelasAmoedo/TelasAmoedo/TelasAmoedo/ViewModels/MenuVoucherViewModel.cs
+++ TelasAmoedo/TelasAmoedo/TelasAmoedo/ViewModels/MenuVoucherViewModel.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
+using TelasAmoedo.Models;
 
 namespace TelasAmoedo.ViewModels
 {
-    public class MenuVoucherViewModel
+    public class MenuVoucherViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<ItemsMenuVoucher> Itens { get; set; }
+
+        public MenuVoucherViewModel()
+        {
+            Itens = new ObservableCollection<ItemsMenuVoucher>();
+            Itens.Add(new ItemsMenuVoucher { Codigo=123456, Valor=250});
+            Itens.Add(new ItemsMenuVoucher { Codigo = 234567, Valor = 150 });
+            Itens.Add(new ItemsMenuVoucher { Codigo = 345678, Valor = 150 });
+            Itens.Add(new ItemsMenuVoucher { Codigo = 456789, Valor = 50 });
+            Itens.Add(new ItemsMenuVoucher { Codigo = 567890, Valor = 250 });
+            Itens.Add(new ItemsMenuVoucher { Codigo = 678901, Valor = 150 });
+        }
          
     }
 }
