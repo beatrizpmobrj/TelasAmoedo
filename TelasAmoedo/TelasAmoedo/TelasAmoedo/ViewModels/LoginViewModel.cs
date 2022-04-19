f8ddd48642ebf60c587ec4a0bf2707b972c14f16
HigorPereira <higor.pereira@mobrj.com.br>
2022-04-19 17:04:03 -0300

    Continuar novo fluxo de login

--- TelasAmoedo/TelasAmoedo/TelasAmoedo/ViewModels/LoginViewModel.cs
+++ TelasAmoedo/TelasAmoedo/TelasAmoedo/ViewModels/LoginViewModel.cs
@@ -16,6 +16,7 @@ namespace TelasAmoedo.ViewModels
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+        public string IsPrimeiroAcesso { get; set; }
         private string _email;
         public string Email
         {
@@ -65,137 +66,184 @@ namespace TelasAmoedo.ViewModels
         {
             string emailValido = "email@mobrj.br";
 
-            var _emailSecureStorage = await Xamarin.Essentials.SecureStorage.GetAsync("email");
-            var _senhaSecureStorage = await Xamarin.Essentials.SecureStorage.GetAsync("senha");
+            string _emailSecureStorage = await Xamarin.Essentials.SecureStorage.GetAsync("email");
+            string _senhaSecureStorage = await Xamarin.Essentials.SecureStorage.GetAsync("senha");
+            string _isPrimeiroAcesso = await Xamarin.Essentials.SecureStorage.GetAsync("isPrimeiroAcesso");
 
-            if(!String.IsNullOrWhiteSpace(Email) && !String.IsNullOrWhiteSpace(Senha)) // Usuário passa email e senha - sem digital
-            {
-                await ValidarLogin(Email, Senha);
-            }
-            else if (!String.IsNullOrWhiteSpace(Email) && String.IsNullOrWhiteSpace(Senha)) // Se passar somente email - com digital
-            {
-                if (Email == emailValido)
+            // Se for primeira vez, usuário opta ou não por logar com biometria
+            if (!String.IsNullOrWhiteSpace(Email) && !String.IsNullOrWhiteSpace(Senha)) // Usuário passa email e senha
+            { 
+                if(Email != _emailSecureStorage) // Se email não for o do secure storage, perguntar se quer salvar
                 {
-                    // Pode virar um método
                     var availability = await CrossFingerprint.Current.IsAvailableAsync();
 
-                    if (!availability)
+                    if (availability)
                     {
-                        await App.Current.MainPage.DisplayToastAsync("Leitor biométrico não disponível.", 3000);
-                        return; // Sai do método
-                    }
+                        var reposta = await App.Current.MainPage.DisplayAlert("Alerta!", "Deseja usar biometria para entrar na próxima vez?", "Sim", "Não");
 
-                    var authResult = await CrossFingerprint.Current.AuthenticateAsync(
-                        new AuthenticationRequestConfiguration("Acesso Biométrico", "Confirme sua impressão digital para acessar sua conta."));
-
-                    if (authResult.Authenticated) // Caso não seja autenticado o próprio pacote possui uma mensagem
-                    {
-                        await Shell.Current.GoToAsync($"//{nameof(MenuPrincipal)}");
+                        if (reposta) // Se o usuário quiser salvar o login
+                        {
+                            IsPrimeiroAcesso = "true";
+                            await SalvarLogin();
+                            await ValidarLogin(Email, Senha);
+                        }
+                        else // Se o usuário não quiser salvar o login
+                        {
+                            await ValidarLogin(Email, Senha);
+                        }
                     }
                 }
-                else
+                else // Se o email for igual ao secure storage, verificar se possui biometria
                 {
-                    await App.Current.MainPage.DisplayToastAsync("Insira seu login corretamente.", 3000);
-                    return;
-                }
-            }
-            else if(String.IsNullOrWhiteSpace(Email) && String.IsNullOrWhiteSpace(Senha)) // Usuário não passa nada, verificar secure storage
-            {
-                if (_emailSecureStorage == emailValido)
-                {
-                    Email = _emailSecureStorage; // Mostra o email na tela
-
-                    // Pode virar um método
                     var availability = await CrossFingerprint.Current.IsAvailableAsync();
 
-                    if (!availability)
-                    {
-                        await App.Current.MainPage.DisplayToastAsync("Leitor biométrico não disponível.", 3000);
-                        return; // Sai do método
-                    }
-
-                    var reposta = await App.Current.MainPage.DisplayAlert("Alerta!", "Deseja usar biometria?", "Sim", "Não");
-
-                    if (reposta)
+                    if (availability) // Se possuir biometria, logar com ela
                     {
                         var authResult = await CrossFingerprint.Current.AuthenticateAsync(
                         new AuthenticationRequestConfiguration("Acesso Biométrico", "Confirme sua impressão digital para acessar sua conta."));
 
-                        if (authResult.Authenticated)
+                        if (authResult.Authenticated) // Caso não seja autenticado o próprio pacote possui uma mensagem
                         {
                             await Shell.Current.GoToAsync($"//{nameof(MenuPrincipal)}");
                         }
                     }
-                    else
+                    else // Se não possuir biometria, entrar com login e senha
                     {
-                        Email = null; // Retira o email da tela
-                        await App.Current.MainPage.DisplayToastAsync("Insira email e senha.", 2000);
                         await ValidarLogin(Email, Senha);
                     }
                 }
-                else
-                {
-                    await App.Current.MainPage.DisplayToastAsync("Usuário não encontrado.", 3000);
-                    return;
-                }
+                
             }
-            else
-            {
-                await App.Current.MainPage.DisplayToastAsync("Insira seu login.", 2000);
-            }
+
+            // Se não for a primeira vez
+            //// Se usuário optou por logar com biometria
+            //// Se usuário optou por não logar com biometria
+
+            //if (!String.IsNullOrWhiteSpace(Email) && !String.IsNullOrWhiteSpace(Senha)) // Usuário passa email e senha - sem digital
+            //{
+            //    await ValidarLogin(Email, Senha);
+            //}
+            //else if (!String.IsNullOrWhiteSpace(Email) && String.IsNullOrWhiteSpace(Senha)) // Se passar somente email - com digital
+            //{
+            //    if (Email == emailValido)
+            //    {
+            //        // Pode virar um método
+            //        var availability = await CrossFingerprint.Current.IsAvailableAsync();
+
+            //        if (!availability)
+            //        {
+            //            await App.Current.MainPage.DisplayToastAsync("Leitor biométrico não disponível.", 3000);
+            //            return; // Sai do método
+            //        }
+
+            //        var authResult = await CrossFingerprint.Current.AuthenticateAsync(
+            //            new AuthenticationRequestConfiguration("Acesso Biométrico", "Confirme sua impressão digital para acessar sua conta."));
+
+            //        if (authResult.Authenticated) // Caso não seja autenticado o próprio pacote possui uma mensagem
+            //        {
+            //            await Shell.Current.GoToAsync($"//{nameof(MenuPrincipal)}");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        await App.Current.MainPage.DisplayToastAsync("Insira seu login corretamente.", 3000);
+            //        return;
+            //    }
+            //}
+            //else if(String.IsNullOrWhiteSpace(Email) && String.IsNullOrWhiteSpace(Senha)) // Usuário não passa nada, verificar secure storage
+            //{
+            //    if (_emailSecureStorage == emailValido)
+            //    {
+            //        Email = _emailSecureStorage; // Mostra o email na tela
+
+            //        // Pode virar um método
+            //        var availability = await CrossFingerprint.Current.IsAvailableAsync();
+
+            //        if (!availability)
+            //        {
+            //            await App.Current.MainPage.DisplayToastAsync("Leitor biométrico não disponível.", 3000);
+            //            return; // Sai do método
+            //        }
+
+            //        var reposta = await App.Current.MainPage.DisplayAlert("Alerta!", "Deseja usar biometria?", "Sim", "Não");
+
+            //        if (reposta)
+            //        {
+            //            var authResult = await CrossFingerprint.Current.AuthenticateAsync(
+            //            new AuthenticationRequestConfiguration("Acesso Biométrico", "Confirme sua impressão digital para acessar sua conta."));
+
+            //            if (authResult.Authenticated)
+            //            {
+            //                await Shell.Current.GoToAsync($"//{nameof(MenuPrincipal)}");
+            //            }
+            //        }
+            //        else
+            //        {
+            //            Email = null; // Retira o email da tela
+            //            await App.Current.MainPage.DisplayToastAsync("Insira email e senha.", 2000);
+            //            await ValidarLogin(Email, Senha);
+            //        }
+            //    }
+            //    else
+            //    {
+            //        await App.Current.MainPage.DisplayToastAsync("Usuário não encontrado.", 3000);
+            //        return;
+            //    }
+            //}
+            //else
+            //{
+            //    await App.Current.MainPage.DisplayToastAsync("Insira seu login.", 2000);
+            //}
             
         }
 
-        private async Task IsDigitalValidadaAsync()
+        //private async Task IsDigitalValidadaAsync()
+        //{
+        //    var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+        //    if (status != PermissionStatus.Granted)
+        //        status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+
+        //    if (status != PermissionStatus.Granted) // Se por qualquer motivo o status for diferente de Granted; caso seja negado o uso
+        //        return;
+
+        //    var availability = await CrossFingerprint.Current.IsAvailableAsync();
+
+        //    var authenticationType = await CrossFingerprint.Current.GetAuthenticationTypeAsync(); // Pegando o tipo para posterior mensagem personalizada
+
+        //    if (!availability)
+        //    {
+        //        await App.Current.MainPage.DisplayToastAsync("Leitor biométrico não disponível.", 5000);
+        //        return;
+        //    }
+
+        //    var authResult = await CrossFingerprint.Current.AuthenticateAsync(
+        //        new AuthenticationRequestConfiguration("Acesso Biométrico", "Confirme sua impressão digital para acessar sua conta."));
+
+        //    if (authResult.Authenticated)
+        //    {
+        //        await App.Current.MainPage.DisplayToastAsync("Acesso liberado!", 5000);
+        //        await Shell.Current.GoToAsync("confirmacaotelefonepage");
+        //    }
+        //}
+        private async Task<string> MostrarLoginAsync()
         {
-            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
-
-            if (status != PermissionStatus.Granted)
-                status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
-
-            if (status != PermissionStatus.Granted) // Se por qualquer motivo o status for diferente de Granted; caso seja negado o uso
-                return;
-
-            var availability = await CrossFingerprint.Current.IsAvailableAsync();
-
-            var authenticationType = await CrossFingerprint.Current.GetAuthenticationTypeAsync(); // Pegando o tipo para posterior mensagem personalizada
-
-            if (!availability)
-            {
-                await App.Current.MainPage.DisplayToastAsync("Leitor biométrico não disponível.", 5000);
-                return;
-            }
-
-            var authResult = await CrossFingerprint.Current.AuthenticateAsync(
-                new AuthenticationRequestConfiguration("Acesso Biométrico", "Confirme sua impressão digital para acessar sua conta."));
-
-            if (authResult.Authenticated)
-            {
-                await App.Current.MainPage.DisplayToastAsync("Acesso liberado!", 5000);
-                await Shell.Current.GoToAsync("confirmacaotelefonepage");
-            }
-        }
-        private async Task MostrarLoginAsync()
-        {
-            await SalvarLogin();
-
             try
             {
                 _email = await Xamarin.Essentials.SecureStorage.GetAsync("email");
-                _senha = await Xamarin.Essentials.SecureStorage.GetAsync("senha");
             }
             catch (Exception)
             {
-                await App.Current.MainPage.DisplayToastAsync("Não encontrado!", 3000);
+                _email = "";
             }
-            await App.Current.MainPage.DisplayAlert("Seu login é:", $"Email: {Email}\r\n Senha: {Senha}", "OK");
+            return _email;
         }
         private async Task SalvarLogin()
         {
             await Xamarin.Essentials.SecureStorage.SetAsync("email", Email);
             await Xamarin.Essentials.SecureStorage.SetAsync("senha", Senha);
-            await App.Current.MainPage.DisplayToastAsync("Login salvo com sucesso!", 3000);
-
+            await Xamarin.Essentials.SecureStorage.SetAsync("isPrimeiroAcesso", IsPrimeiroAcesso);
+            //await App.Current.MainPage.DisplayToastAsync("Login salvo com sucesso!", 3000);
         }
         public async Task ValidarLogin(string email, string senha)
         {
