7baab20caf57382202d7a329f49bdd1cf56cceb3
Beatriz Pinheiro <beatriz.pinheiro@mobrj.com.br>
2022-04-20 15:13:10 -0300

    Arredondando cantos da tabbar usando ShellRenderer

---
+++ TelasAmoedo/TelasAmoedo/TelasAmoedo.Android/MyShellBottomNavViewAppearanceTracker.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Google.Android.Material.BottomNavigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace TelasAmoedo.Droid
+{
+    public class MyShellBottomNavViewAppearanceTracker : IShellBottomNavViewAppearanceTracker
+    {
+        public void Dispose()
+        {
+        }
+
+        public void ResetAppearance(BottomNavigationView bottomView)
+        {
+        }
+
+        public void SetAppearance(BottomNavigationView bottomView, IShellAppearanceElement appearance)
+        {
+
+            bottomView.SetBackgroundResource(Resource.Drawable.bottombackground);
+        }
+    }
+}
---
+++ TelasAmoedo/TelasAmoedo/TelasAmoedo.Android/ShellRenderer.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TelasAmoedo;
+using TelasAmoedo.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MainPage), typeof(MyShellRenderer))]
+namespace TelasAmoedo.Droid
+{
+    public class MyShellRenderer : ShellRenderer
+    {
+        public MyShellRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override IShellBottomNavViewAppearanceTracker CreateBottomNavViewAppearanceTracker(ShellItem shellItem)
+        {
+            return new MyShellBottomNavViewAppearanceTracker();
+        }
+    }
+}
